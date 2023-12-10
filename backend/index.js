5990b1a5e13f52f622896a1921df115f62de108b
Amir Begic <amir.begic12@gmail.com>
2023-12-10 04:24:18 +0100

    cors modified

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,26 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import bookRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
 // Middleware za prosljedjivanje eq body-a
 app.use(express.json());
+
+// Middleware za handlanje CORS POLICY
+// 1 option dozvola za sve origins sa default cors(*)
+app.use(cors());
+
+// 2 option Allow custom origins
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.get("/", (req, res) => {
   console.log(req);
