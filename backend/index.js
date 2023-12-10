43a60e82cc7d77459751353c1caa007fd1a97e9d
Amir Begic <amir.begic12@gmail.com>
2023-12-10 03:16:12 +0100

    connected to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,23 @@
 import express from "express";
-import { PORT} from "./config.js";
+import { PORT, mongoDBURL} from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(PORT, () =>{
-    console.log(`App is listening to port: ${PORT}`);
+app.get('/', (req, res)=>{
+    console.log(req)
+    return res.status(234).send('Hello');
 });
 
+
+
+mongoose.connect(mongoDBURL)
+    .then(()=>{
+        app.listen(PORT, () =>{
+            console.log(`App is listening to port: ${PORT}`);
+        });
+        console.log('App connected to database');
+    })
+    .catch((error) =>{
+        console.log(error);
+    });
